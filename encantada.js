f3bcf6e4f53a03a5c1a12b4e5b9189c15230d7f8
StanleyTC-Professional <rampeustiusken3@gmail.com>
2023-06-19 20:13:51 -0300

    first command jokes

--- encantada.js
+++ encantada.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
-
+const piadas = require('./piadas.js');
+require('dotenv').config();
+const token = process.env.TOKEN;
 
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
@@ -7,7 +9,47 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Part
 
 client.once('ready', () => {
     console.log('Ready!');
+
+    const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const commands = [
+  {
+    name: 'piada',
+    description: 'Obtenha uma piada engraçada do bot.',
+  },
+];
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(client.user.id),
+      { body: commands },
+    );
+
+
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+
+      
 });
 
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;   
+  
+    const { commandName } = interaction;
+  
+    if (commandName === 'piada') {
+      // Lógica para obter uma piada engraçada (você pode usar uma API ou um array de piadas)
+      const joke = piadas[Math.floor(Math.random() * piadas.length)];
+      await interaction.reply(joke);
+    }
+  });
+  
 
-client.login('');
+client.login(token);
---
+++ piadas.js
@@ -0,0 +1,7 @@
+const piadas = [
+    '-Oi, meu nome é Jaqueline, eu tenho 12 anos e já namoro \n-Já o que???? \n-Queline',
+    'Vocês serão exterminados dia 21 de novembro =D',
+    '-Capitão Cavalcante, Sentila Matos falando, estou observando uma tropa de soldasdos se dirigindo para a base\n -Afirmativo sentinela, eles são amigos ou inimigos?\n-Eu acho que são amigos, eles estão vindo todos juntos',
+];
+
+module.exports = piadas;
