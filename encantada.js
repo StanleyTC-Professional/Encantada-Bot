31c5b5e0e8e6d2bda0b0afd3868292ef65686d8e
StanleyTC-Professional <rampeustiusken3@gmail.com>
2023-06-20 20:52:03 -0300

    ping command

--- encantada.js
+++ encantada.js
@@ -13,11 +13,15 @@ client.once('ready', () => {
     const { REST } = require('@discordjs/rest');
     const { Routes } = require('discord-api-types/v9');
 
-    const commands = [
+    const commands = [ // Irei colocar todos os comandos em uma array com esse nome, onde cada comando será um objeto {}, com chave-valor, no caso name-description
     {
         name: 'piada',
         description: 'Obtenha uma piada engraçada do bot.',
     },
+    {
+        name:'ping',
+        description: 'Obtenha o ping do bot'
+    }
     ];
 
     const rest = new REST({ version: '9' }).setToken(token);
@@ -46,8 +50,14 @@ client.on('interactionCreate', async (interaction) => {
   
     if (commandName === 'piada') {
       // Lógica para obter uma piada engraçada (você pode usar uma API ou um array de piadas)
-      const joke = piadas[Math.floor(Math.random() * piadas.length)];
-      await interaction.reply(joke);
+        const joke = piadas[Math.floor(Math.random() * piadas.length)]; // ele percorrerá uma lista que é a lista de piadas e escolherá uma aleatória com math.random()
+        await interaction.reply(joke);
+    }
+
+    if (commandName === 'ping'){
+        const ping = '${Math.round(client.ws.ping)}ms';
+        await interaction.reply(ping);
+        
     }
   });
   
