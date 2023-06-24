4d43eff641bad5f2a63165aa520edfcd275e3465
StanleyTC-Professional <rampeustiusken3@gmail.com>
2023-06-24 11:32:28 -0300

    new commands in python to answers

--- encantada.py
+++ encantada.py
@@ -3,22 +3,33 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv('TOKEN')
+from random import randint
+import respostas
 
 
 
-
-intents = discord.Intents.default()
-intents.guilds = True
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged on as {client.user}!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('encantada'):
+        resposta = respostas.sortear(randint(1, 10), message.author)
+        await message.channel.send(resposta)
+
+
+
+
 client.run(token)
---
+++ respostas.py
@@ -0,0 +1,23 @@
+def sortear(sorteio, usuario):
+
+    if sorteio == 1:
+        return('Opa! bão?')
+    if sorteio == 2:
+        return('oiiiiiiiiiiiii')
+    if sorteio == 3:
+        return('qual a boa?')
+    if sorteio == 4:
+        return('ola ><')
+    if sorteio == 5:
+        return(' oiii :3 ')
+    if sorteio == 6:
+        return('Opa!')
+    if sorteio == 7:
+        return(f'Manda haver {usuario}!')
+    if sorteio == 8:
+        return(f'Boa noite {usuario}!')
+    if sorteio == 9:
+        return(f'Pode falar {usuario}!')
+    if sorteio == 10:
+        return(f'Oeeeeeee {usuario}')
+
